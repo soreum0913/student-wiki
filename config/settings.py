ad93f461979926b37a4a58cdc6cb5c11056b48b5
soreum0913 <nympe09@gmail.com>
2025-06-04 14:40:12 +0900

    qna, wiki app add

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "qna.apps.QnaConfig",
+    "wiki.apps.WikiConfig"
 ]
 
 MIDDLEWARE = [
