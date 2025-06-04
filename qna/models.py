efbc3f982616e03ee8dc99972172935df495b980
soreum0913 <nympe09@gmail.com>
2025-06-04 15:12:12 +0900

    wrote qna model

--- qna/models.py
+++ qna/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    votes = models.IntegerField(default=0)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    recommend = models.IntegerField(default=0)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Answer to {self.question.title}"
+
--- qna/views.py
+++ qna/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def question_list(request):
+    questions = Question.objects.all().annotate(answer_count=Count('answers')).order_by('-votes')
+    return render(request, 'qna/question_list.html', {'questions': questions})
+
